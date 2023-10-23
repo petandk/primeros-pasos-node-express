83471b094337fa169ff57e317d3bf28307f9cd47
Raúl Manzanas Trillo <genyo_liceo@hotmail.com>
2023-10-23 10:35:10 +0200

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import Express from "express";
+
+const app: Express.Application = Express();
+const PORT: number = 3000;
+
+app.get("/", (req: Express.Request, res: Express.Response)=>{
+   res.send("<p>Hola mundo!</p>");
+});
+
+app.listen(PORT, ()=>{
+    console.log(`App listening on port: ${PORT}`);
+});
